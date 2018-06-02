4f07af47568d3b78169956ce97812d6c942e5c87
Kylie Jue <kjue25@gmail.com>
2018-06-02 12:59:20 -0700

    Add data structures + logic

--- education-equity.js
+++ education-equity.js
@@ -1,3 +1,10 @@
-function next(panel) {
-	console.log(panel);
+let VALUE_STATEMENTS = [];
+let SCORES = new Map([[0,[[-1,0,0,0,0,0], [1,0,0,0,0,0]]], [1, [[],[]]], [2, [[],[]]]]);
+let user_scores = [];
+
+function next(clicked_panel) {
+	for (let i = 0; i < 6; i++) {
+		user_scores[i] += SCORES[curr_index][clicked_panel][i];
+	}
+	console.log(clicked_panel);
 }
