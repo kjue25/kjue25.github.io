ac2938798244712c31b9f547ffceb9652785e8ec
jacob wolf <jacob.h.wolf@gmail.com>
2018-06-03 16:32:20 -0700

    updated constants to reflect user testing feedback about content of statements

--- education-equity-constants.js
+++ education-equity-constants.js
@@ -4,9 +4,9 @@ let INSTRUCTIONS = ["For the following questions, choose the option which best d
 
 let VALUE_STATEMENTS = [["I believe all students in a classroom should receive equal attention from the teacher.", "I believe students who are struggling should receive more attention from the teacher."],
 						["I believe schools should have different classes for advanced and underperforming students.", "I believe that students should be in heterogeneous classrooms that cater to all abilities."],
-						["I believe quantitative metrics like test scores and college admission rates are better measures of school quality than environmental factors.", "I believe environmental factors like racial and ethnic diversity are better measures of school quality than quantitative metrics like test scores."], 
+						["I believe quantitative metrics like test scores should be weighted higher than environmental factors when assessing school success.", "I believe environmental factors like school culture should be weighted higher than quantitative metrics like test scores when assessing school success."], 
 						["I believe that in education everyone should benefit equally regardless of their financial contributions.", "I believe that in education those who contribute the most money should benefit the most."], 
-						["I believe that it is important to prioritize creating racially and ethnically diverse schools.", "I believe that it is important to prioritize academic achievement over racial and ethnic diversity."], 
+						["I believe that it is important to prioritize racial and ethnic diversity in schools over academic achievement.", "I believe that it is important to prioritize academic achievement over racial and ethnic diversity."], 
 						["I believe that suspension should be allowed as a form of discipline in schools.", "I believe that suspension should not be allowed as a form of discipline in schools."], 
 						["I believe it is the job of policymakers to create policies that promote racial integration in schools.", "I believe that policymakers are not responsible for creating policies around racial integration in schools."], 
 						["I believe that schools should prioritize academic rigor.", "I believe that schools should prioritize students’ happiness and emotional well-being."]];
@@ -15,11 +15,11 @@ let VALUE_STATEMENTS = [["I believe all students in a classroom should receive e
 //increments in each user_score category for choices of left or right value statement
 let SCORES = new Map([[0,[[-1,0,0,0,0,0], [1,0,0,0,0,0]]], 
 					  [1, [[0,-1,0,0,0,0],[0,1,0,0,0,0]]], 
-					  [2, [[0,0,-1,0,0,0],[0,0,1,1,0,0]]], 
+					  [2, [[0,0,-1,0,0,0],[0,0,1,0,0,0]]], 
 					  [3, [[0,0,0,0,1,0,],[0,0,0,0,-1,0]]],
-					  [4, [[0,0,0,1,0,0],[0,0,-1,-1,0,0]]],
+					  [4, [[0,0,1,1,0,0],[0,0,-1,-1,0,0]]],
 					  [5, [[0,0,0,0,0,-1],[0,0,0,0,0,1]]],
-					  [6, [[0,0,0,1,0,0],[0,0,0,0,0,0]]],
+					  [6, [[0,0,0,2,0,0],[0,0,0,0,0,0]]],
 					  [7, [[0,0,-1,0,0,0],[0,0,0,0,0,0]]]]);
 
 // Corresponding indices to score category for equitable_statements, user_score, SCHOOL_EQUITY_CATEGORIES:
@@ -35,23 +35,29 @@ let SCHOOL1 = new Map([['school type', 'public school'],
 						['demographics', 'Majority Asian and white students, with 10% black and Latinx students'],
 						['discipline', 'Students cannot be suspended; instead, they are required to attend counseling services.'],
 						['academics', 'Below average test scores'],
-						['tracking', 'After 1st grade, students are separated into tracks based on grades; ability to switch tracks each year given standardized test performance.'],
-						['support programs', 'Provides bare minimum for legally required support services, such as counseling; however, it is reserved for those who need it the most, and may not be of the best quality.'],
-						['class size', '20-25 students / class']]);
+						['tracking', 'After 1st grade, students are separated into classes based on academic performance; students can switch tracks each year given standardized test performance.'],
+						['support programs', 'Provides bare minimum for legally required support services, such as mental and physical health counseling and disability accommodations. There is one counselor for the entire school.'],
+						['supplemental programs', 'No organized art or music program.'],
+						['class size', '20-25 students / teacher'],
+						['school size', '486']]);
 let SCHOOL2 = new Map([['school type', 'Public charter school with private K to guarantee admission'], 
 						['demographics', 'Majority black and Latinx students'],
 						['discipline', 'Disciplinary process includes meetings with parents and staff process; suspension occurs after third disciplinary infraction.'],
 						['academics', 'Average test scores'],
 						['tracking', 'Students are primarily randomly assigned; gifted ed program can be tested into.'],
-						['support programs', 'Provides basic support mechanisms for students, including mental and physical health services, as well as a variety of disabilities supports.'],
-						['class size', '25+ students / class']]);
+						['support programs', 'Good quality support services, such as mental and physical health counseling and disability accommodations. There are 4 school counselors.'],
+						['supplemental programs', 'After school music lessons that parents can opt in and pay for.'],
+						['class size', '25+ students / teacher'],
+						['school size', '553']]);
 let SCHOOL3 = new Map([['school type', 'Private school with sufficient need-based financial aid to support your family.'], 
 						['demographics', 'Majority white students, with 2% black and Latinx students'],
 						['discipline', 'Disciplinary process includes meetings with parents and staff, and suspension as a last resort.'],
 						['academics', 'Above average test scores'],
 						['tracking', 'Students are randomly assigned to classes.'],
-						['support programs', 'Strong support mechanisms for struggling students to receive additional support through tutoring, personalized advising, test prep courses, etc.'],
-						['class size', '15-20 students / class']]);
+						['support programs', 'High quality support services, such as mental and physical health counseling and disability accommodations. There is a specialized team of 10 support personnel who provide hyper individualized care for each student.'],
+						['supplemental programs', 'Has arts and music program with specialized facilities and instructors.'],
+						['class size', '15-20 students / teacher'],
+						['school size', '204']]);
 
 //array of schools
 let SCHOOLS = [SCHOOL1, SCHOOL2, SCHOOL3];
@@ -70,17 +76,19 @@ let SCHOOL_PROPERTY_TO_INDEX_MAP = new Map([[0, 'school type'],
 											[3, 'academics'],
 											[4, 'tracking'],
 											[5, 'support programs'],
-											[6, 'class size']]);
+											[6, 'supplemental programs'],
+											[7, 'class size'],
+											[8, 'school size']]);
 
 //Value category keys that map to arrays of school property flags defined by the SCHOOL_PROPERTY_TO_INDEX_MAP above. If property is flagged, 
 //it indicates that the property relates to the equity category and should be flagged if there is a school choice/equity category mismatch.
 
-let CATEGORY_TO_SCHOOL_PROPERTIES = new  Map([['support equity', [0,0,0,0,0,1,1]],
-											 ['opportunity equity', [0,0,0,0,1,0,1]],
-											 ['testing', [0,0,0,1,0,0,0]],
-											 ['resource equity', [1,0,0,0,0,0,0]],
-											 ['diversity', [0,1,0,0,0,0,0,0]],
-											 ['discipline', [0,0,1,0,0,0,0]]]);
+let CATEGORY_TO_SCHOOL_PROPERTIES = new  Map([['support equity', [0,0,0,0,0,1,0,0,0]],
+											 ['opportunity equity', [0,0,0,0,1,0,0,0,0]],
+											 ['testing', [0,0,0,1,0,0,0,0,0]],
+											 ['resource equity', [1,0,0,0,0,0,1,0,0]],
+											 ['diversity', [0,1,0,0,0,0,0,0,0]],
+											 ['discipline', [0,0,1,0,0,0,0,0,0]]]);
 
 let NUM_STATEMENT_PANELS = 8;
 let NUM_VALUE_CATEGORIES = 6;
