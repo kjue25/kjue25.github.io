8dff820b7cbf59f03ab5aceeb144a1daa4301b16
jennylihan <jlhan@stanford.edu>
2018-06-03 13:07:47 -0700

    background

--- education-equity.js
+++ education-equity.js
@@ -9,15 +9,15 @@ let INSTRUCTIONS = ["For the following questions, choose the option which best d
 					"You are trying to choose a school for your child and have a few options:"];
 let VALUE_STATEMENTS = [["I believe all students in a classroom should receive equal attention from the teacher.", "I believe students who are struggling should receive more attention from the teacher."],
 						["I believe schools should have different classes for advanced and underperforming students.", "I believe that students should be in heterogeneous classrooms that cater to all abilities."],
-						["I believe quantitative metrics like test scores and college admission rates are better measures of school quality than environmental factors.", "I believe environmental factors like racial and ethnic diversity are better measures of school quality than quantitative metrics like test scores."], 
-						["I believe that in education everyone should benefit equally regardless of their financial contributions.", "I believe that in education those who contribute the most money should benefit the most."], 
-						["I believe that it is important to prioritize creating racially and ethnically diverse schools.", "I believe that it is important to prioritize academic achievement over racial and ethnic diversity."], 
-						["I believe that suspension should be allowed as a form of discipline in schools.", "I believe that suspension should not be allowed as a form of discipline in schools."], 
-						["I believe it is the job of policymakers to create policies that promote racial integration in schools.", "I believe that policymakers are not responsible for creating policies around racial integration in schools."], 
+						["I believe quantitative metrics like test scores and college admission rates are better measures of school quality than environmental factors.", "I believe environmental factors like racial and ethnic diversity are better measures of school quality than quantitative metrics like test scores."],
+						["I believe that in education everyone should benefit equally regardless of their financial contributions.", "I believe that in education those who contribute the most money should benefit the most."],
+						["I believe that it is important to prioritize creating racially and ethnically diverse schools.", "I believe that it is important to prioritize academic achievement over racial and ethnic diversity."],
+						["I believe that suspension should be allowed as a form of discipline in schools.", "I believe that suspension should not be allowed as a form of discipline in schools."],
+						["I believe it is the job of policymakers to create policies that promote racial integration in schools.", "I believe that policymakers are not responsible for creating policies around racial integration in schools."],
 						["I believe that schools should prioritize academic rigor.", "I believe that schools should prioritize students’ happiness and emotional well-being."]];
-let SCORES = new Map([[0,[[-1,0,0,0,0,0], [1,0,0,0,0,0]]], 
-					[1, [[0,-1,0,0,0,0],[0,1,0,0,0,0]]], 
-					[2, [[0,0,1,0,0,0],[0,0,-1,1,0,0]]], 
+let SCORES = new Map([[0,[[-1,0,0,0,0,0], [1,0,0,0,0,0]]],
+					[1, [[0,-1,0,0,0,0],[0,1,0,0,0,0]]],
+					[2, [[0,0,1,0,0,0],[0,0,-1,1,0,0]]],
 					[3, [[0,0,0,0,1,0,],[0,0,0,0,-1,0]]],
 					[4, [[0,0,0,1,0,0],[0,0,1,-1,0,0]]],
 					[5, [[0,0,0,0,0,-1],[0,0,0,0,0,1]]],
@@ -62,7 +62,7 @@ function start() {
 
 // lol need to fix all the button names/make these methods clearer...
 // there's probably a better way to do this than three separate buttons
-// The following four buttons show and hide the relevant instruction buttons 
+// The following four buttons show and hide the relevant instruction buttons
 // and update the instruction div text
 function showNextInstructions() {
 	$('#instructions').hide();
@@ -126,6 +126,7 @@ function chooseSchool(num) {
 
 window.onload = function(){
 	start();
+	HTMLCanvasElement.getContext()
 };
 
 
