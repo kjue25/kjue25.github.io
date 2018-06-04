4afbfe573a548325df6b5d4e851381c5ce8fd3f9
jennylihan <jlhan@stanford.edu>
2018-06-03 17:52:14 -0700

    hides 3 buttons at start

--- education-equity.js
+++ education-equity.js
@@ -15,6 +15,7 @@ let curr_panel_index = 0; // Index to track current value statement panel
 // Called at the start of the program to hide html elements
 function start() {
 	$('#instructions').text(INSTRUCTIONS[0]);
+	$('#intro_instructions_button').show();
 	$('#parent_instructions_button').hide();
 	$('#display_schools_button').hide();
 	$('.overlay-parent').hide();
@@ -114,7 +115,7 @@ function generateSchoolPropHtml(prop, data) {
 function chooseSchool(num) {
 	// $('#instructions').show();
 	// $('#instructions').text("You chose school " + SCHOOL_NAMES[num]);
-	
+
 	// Just hide the school columns you didn't choose
 	for (let i = 0; i < SCHOOLS.length; i++) {
 		if (i !== num) {
